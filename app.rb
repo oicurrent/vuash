50d96e75a7d0ee375d7dd545b486dfd9eb6278c6
Jonathan Duarte <jonathan.duarte@rocketmail.com>
2013-11-30 20:35:03 -0200

    Autodestroy the message

--- app.rb
+++ app.rb
@@ -2,10 +2,13 @@ require 'sinatra'
 require 'active_record'
 require 'securerandom'
 
-ActiveRecord::Base.configurations = { 'memory' => { adapter: 'sqlite3', database: ":memory:" } }
+ActiveRecord::Base.configurations = { 'memory' => { adapter: 'sqlite3', database: "memory.db" } }
 ActiveRecord::Base.establish_connection(ActiveRecord::Base.configurations["memory"])
 
-load 'schema.rb'
+# Essa linha deve ser descomentada na primeira vez que o scrip rodar
+# Apos isso ela deve ser comentada, pq o active record ja tera criada a tabela
+# TODO: Fix it
+# load 'schema.rb'
 
 class Message < ActiveRecord::Base
 end
@@ -18,9 +21,20 @@ post '/' do
   @message = Message.new(info: params['info'], uuid: SecureRandom.hex)
 
   if @message.save
-    haml :show
+    haml :preview
   else
     'Something goes wrong'
+  end
+end
+
+get '/messages/:uuid' do
+  message = Message.find_by(uuid: params['uuid'])
+  if message
+    @info = message.info
+    message.destroy
+    haml :show
+  else
+    haml :removed
   end
 end
 
@@ -40,14 +54,36 @@ __END__
   %br
   %input(type= 'submit' value='Enviar')
 
-@@ show
-%div
-  %h1
-    Mensagem criada com sucesso!
+@@ preview
+%h1
+  Mensagem criada com sucesso!
 
-  %br
-  = @message.info
-  %br
-  Link:
-  %a(href="http://localhost:9393/messages/#{@message.uuid}")
-    http://localhost:9393/messages/#{@message.uuid}
+%br
+= @message.info
+%br
+Link:
+%a(href="http://localhost:9393/messages/#{@message.uuid}")
+  http://localhost:9393/messages/#{@message.uuid}
+
+%br
+%a(href="/")
+  Voltar
+
+@@ show
+%h1
+  Essa mensagem se auto-destruiu
+
+%br
+= @info
+
+%br
+%a(href="/")
+  Voltar
+
+@@ removed
+%h1
+  Mensagem removida
+
+%br
+%a(href="/")
+  Voltar
