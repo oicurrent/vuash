89324878d612c5a1e9ac8e53316cd1f4e57d82d2
Daniel Libanori <daniellibanori@gmail.com>
2013-12-01 15:57:49 -0200

    Parse DATABASE_URL to configure database

--- arbase.rb
+++ arbase.rb
@@ -1,2 +1,24 @@
-ActiveRecord::Base.configurations = { 'vuash' => { adapter: 'sqlite3', database: "db/vuash.db" } }
-ActiveRecord::Base.establish_connection(ActiveRecord::Base.configurations["vuash"])
+require 'uri'
+
+configuration = {}
+
+uri = URI.parse ENV['DATABASE_URL'] || "sqlite3:///db/vuash.sqlite3"
+
+options = {}
+
+(uri.query || "").split("&").each do |param|
+  key, value = param.split("=")
+  options[key.intern] = value
+end
+
+configuration = {
+  adapter: uri.scheme,
+  database: File.join(".", uri.path),
+  host: uri.host,
+  user: uri.user,
+  password: uri.password
+}
+
+configuration.merge! options
+
+ActiveRecord::Base.establish_connection(configuration)
