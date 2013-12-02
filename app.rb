c185ef46e4d0fbbb39bc541bd63cba4e4ed787e4
Daniel Libanori <daniellibanori@gmail.com>
2013-12-02 01:19:25 -0200

    Move model to app

--- app.rb
+++ app.rb
@@ -3,7 +3,9 @@ require 'active_record'
 require 'securerandom'
 
 require_relative 'db/config'
-require_relative 'models/message'
+
+class Message < ActiveRecord::Base
+end
 
 get '/' do
   haml :index
