aa6a2927c11ec1474f5d86ea0abbdde28eeda9f8
Jonathan Duarte <jonathan.duarte@rocketmail.com>
2013-12-09 22:10:51 -0200

    Add copy to clipboard option

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,5 +1,16 @@
 $(function() {
-  $('.js-link').on('click', function(){
+  var clip = new ZeroClipboard($("#copy-button"), {
+      moviePath: "/swfs/ZeroClipboard.swf"
+  });
+
+  clip.on("load", function(client) {
+    client.on("complete", function(client, args) {
+      $('#to-copy').trigger('click')
+      console.log("Copied text to clipboard: " + args.text );
+    });
+  });
+
+  $('#to-copy').on('click', function(){
     $(this).focus()
     $(this).select()
   });
