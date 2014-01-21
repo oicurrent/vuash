6bf086b0399290e7daa8a079626befaf7542440a
Daniel Libanori <daniellibanori@gmail.com>
2014-01-21 02:40:46 -0200

    Add recipes to Sunzi
    - nginx
    - postgresql
    - rbenv and ruby

--- sunzi/install.sh
+++ sunzi/install.sh
@@ -8,10 +8,6 @@ source recipes/sunzi.sh
 # Remove if you're not on Debian/Ubuntu.
 export DEBIAN_FRONTEND=noninteractive
 
-# Add Dotdeb repository. Recommended if you're using Debian. See http://www.dotdeb.org/about/
-# source recipes/dotdeb.sh
-# source recipes/backports.sh
-
 # Update apt catalog and upgrade installed packages
 sunzi.mute "apt-get update"
 sunzi.mute "apt-get -y upgrade"
@@ -25,26 +21,7 @@ if sunzi.install "sysstat"; then
   /etc/init.d/sysstat restart
 fi
 
-# Set RAILS_ENV
-environment=<%= @attributes.environment %>
-
-if ! grep -Fq "RAILS_ENV" ~/.bash_profile; then
-  echo 'Setting up RAILS_ENV...'
-  echo "export RAILS_ENV=$environment" >> ~/.bash_profile
-  source ~/.bash_profile
-fi
-
-# Install Ruby using RVM
-source recipes/rvm.sh
-ruby_version=<%= @attributes.ruby_version %>
-
-if [[ "$(which ruby)" != /usr/local/rvm/rubies/ruby-$ruby_version* ]]; then
-  echo "Installing ruby-$ruby_version"
-  # Install dependencies using RVM autolibs - see https://blog.engineyard.com/2013/rvm-ruby-2-0
-  rvm install --autolibs=3 $ruby_version
-  rvm $ruby_version --default
-  echo 'gem: --no-ri --no-rdoc' > ~/.gemrc
-
-  # Install Bundler
-  gem install bundler
-fi
+source recipes/postgresql.sh
+source recipes/nginx.sh
+source recipes/rbenv.sh
+source recipes/ruby.sh <%= @attributes.ruby_version %>
