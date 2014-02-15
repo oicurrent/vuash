1af323f0e58cd1f8927e07c5522c57d7470cb98f
Daniel Libanori <daniellibanori@gmail.com>
2014-02-15 21:56:35 -0200

    Try to make unicorn work at produciton

--- config/deploy.rb
+++ config/deploy.rb
@@ -41,7 +41,9 @@ namespace :deploy do
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
       within release_path do
+        puts '---'
         execute './unicorn -D'
+        puts '---'
       end
     end
   end
