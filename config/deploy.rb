aa98cdab5cc7dac11d0188b4173bdbf4a0f6fe5b
Daniel Libanori <daniellibanori@gmail.com>
2014-02-16 11:59:00 -0300

    Add tmp to linked dir

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,7 @@ set :linked_files, %w{.env}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-set :linked_dirs, %w{bin log}
+set :linked_dirs, %w{bin log tmp}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
