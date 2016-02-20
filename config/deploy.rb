bbafecdb2801b5eae35975f8e320edcbb05d92cf
Jonathan Duarte <jonathan.duarte@rocketmail.com>
2016-02-20 13:12:32 +0000

    Remove restart for now

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,7 +40,7 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      within(release_path) { execute "./unicorn" }
+      # within(release_path) { execute "./unicorn" }
     end
   end
 
