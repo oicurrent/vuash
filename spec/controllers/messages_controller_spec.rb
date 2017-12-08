707db190b2e0985611dfec97b29c9a631fd0ebdd
Hugo Abonizio <hugo_abonizio@hotmail.com>
2017-12-08 10:04:04 -0200

    Add support for JSON in messages#create and messages#destroy

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,37 +1,65 @@
 require 'rails_helper'
 
 RSpec.describe MessagesController, type: :controller do
+  context 'HTML' do
+    describe 'GET #new' do
+      it 'returns HTTP success' do
+        get :new
+        expect(response).to have_http_status(:success)
+      end
+    end
 
-  describe 'GET #new' do
-    it 'returns http success' do
-      get :new
-      expect(response).to have_http_status(:success)
+    describe 'POST #create' do
+      it 'returns HTTP success' do
+        post :create, message: { data: '....' }
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    describe 'GET #show' do
+      before { allow(Message).to receive(:find) { double }}
+
+      it 'returns HTTP success' do
+        get :show, id: 1
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    describe 'DELETE #destroy' do
+      before { allow(Message).to receive(:destroy) { double }}
+
+      it 'returns HTTP success' do
+        delete :destroy, id: 1
+        expect(response).to have_http_status(:success)
+      end
     end
   end
 
-  describe 'POST #create' do
-    it 'returns http success' do
-      post :create, message: { data: '....' }
-      expect(response).to have_http_status(:success)
+  context 'JSON' do
+    describe 'POST #create' do
+      it 'returns HTTP success' do
+        post :create, message: { data: '....' }, format: :json
+        body = JSON.parse(response.body)
+        expect(response).to have_http_status(:success)
+        expect(body['id'].size > 0).to be true
+      end
+    end
+
+    describe 'DELETE #destroy' do
+      it 'returns raw data' do
+        m = Message.create(data: '...')
+        delete :destroy, id: m.id, format: :json
+        body = JSON.parse(response.body)
+        expect(response).to have_http_status(:success)
+        expect(body['data'].size > 0).to be true
+      end
+
+      it "returns error when message doesn't exist" do
+        delete :destroy, id: 'INXESISTENT', format: :json
+        body = JSON.parse(response.body)
+        expect(response).to have_http_status(:success)
+        expect(body['error']).to eq 'Not found'
+      end
     end
   end
-
-  describe 'GET #show' do
-    before { allow(Message).to receive(:find) { double }}
-
-    it 'returns http success' do
-      get :show, id: 1
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe 'DELETE #destroy' do
-    before { allow(Message).to receive(:destroy) { double }}
-
-    it 'returns http success' do
-      delete :destroy, id: 1
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
